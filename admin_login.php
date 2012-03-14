4599f7600667e749936775cd52a00ad70605ebbb
Ant0sh <antoshby@gmail.com>
2012-03-14 16:04:23 +0300

    мелкие изменения

--- admin_login.php
+++ admin_login.php
@@ -5,5 +5,6 @@ $smarty = new Smarty;
 $smarty->template_dir = 'templates/';
 $smarty->template_c = 'templates_c/';
 
+
 $smarty->assign('title', '¬ход в административную панель.');
 $smarty->display('admin_login.tpl');
--- contacts.php
+++ contacts.php
@@ -1,8 +1,1 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: User
- * Date: 14.03.12
- * Time: 8:31
- * To change this template use File | Settings | File Templates.
- */
